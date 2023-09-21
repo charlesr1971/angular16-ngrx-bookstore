0a7e932d7592943074179534dbdaee1471cb8f3c
Charles Robertson <cdesign@btinternet.com>
2023-09-21 17:16:24 +0000

    Update 8 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
 
-import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
+import { SharedModule } from './shared/shared.module';
 import { TechnicalTestModule } from './components/technical-test/technical-test.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    SharedModule,
     TechnicalTestModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    BrowserAnimationsModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
